e0354e40dd094b13b9cb55cbac28d10f1c01156e
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-20 23:30:07 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,8 +5,9 @@ def load_library(path)
   # code goes here
   hash = YAML.load_file(path)
   library = Hash.new()
+  library["get_emoticon"] = {}
   hash.each do |meaning, emoticons|
-    
+    library["get_emoticon"].merge!({emoticons[0] => emoticons[1]})
   end 
 end
 
